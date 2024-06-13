a6dcbd9e25d78313e3107a85183729d8693e3c82
Mitchap <mitchbarcenilla4@gmail.com>
2024-06-13 19:14:43 +0800

    homepage updated

--- resources/views/userPages/homepage.blade.php
+++ resources/views/userPages/homepage.blade.php
@@ -157,17 +157,9 @@
                     <div class="p-20 max-w-[100rem] mx-auto">
                         <span class="text-4xl "><i class="fa-solid fa-quote-left"></i></span> 
                         <p class="text-pretty leading-8">
-                            Auf der Heide blüht
-                            ein kleines Blümelein
-                            Und das heißt: Erika.
-                            Heiß von hunderttausend
-                            kleinen Bienelein
-                            Wird umschwärmt Erika.
-                            Denn ihr Herz ist voller Süßigkeit,
-                            Zarter Duft entströmt dem Blütenkleid
-                            Auf der Heide blüht
-                            ein kleines Blümelein
-                            Und das heißt: Erika.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
                         </p>
                         <span class="text-4xl "><i class="fa-solid fa-quote-right"></i></span> 
 
