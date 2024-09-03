e436bf3def5270334682ba4a123a65e3f4300bcb
Mitchap <mitchbarcenilla4@gmail.com>
2024-09-03 15:32:50 +0800

    added edit and update functionality for program events

--- resources/views/adminPages/editEvent.blade.php
+++ resources/views/adminPages/editEvent.blade.php
@@ -4,7 +4,7 @@
 @include('navbars.admin-navbar')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-<div class="-mt-3 md:-mt-1 mb-40 overflow-x-hidden">
+<div class="mt-3 md:-mt-1 mb-40 overflow-x-hidden">
 
   <section class="mb-14">
       <div class="flex flex-col items-center justify-center gap-10 mb-20">
@@ -26,15 +26,15 @@
                     <label for="role" class="form-label font-sans font-medium capitalize">Role</label>
                     <input type="text" class="form-control border-0 bg-gray-300 rounded" name="eventDate" id="eventDate"  value="{{ $eventData -> eventDate }}" required>
                 </div>
-                <div class="gap-4 modal-footer">
-                    <a href="programs_management" type="button" class="btn btn-danger">Cancel</a>
-                    <button type="submit" class="btn bg-lime-600 hover:bg-lime-700 text-neutral-100 hover:text-neutral-100">Save</button>
-                </div>
+
                 <div class="mb-3 flex flex-col justify-center items-center">
                     <img id="showPhoto" class="rounded-lg w-40 mb-4" src="{{ asset($eventData->eventImage) }}" alt="photo"/>
                     <input type="file" name="photo" id="photo" class="" onchange="previewImage(this);" accept=".jpg, .png, .jpeg">
                 </div>
-
+                <div class="gap-4 modal-footer">
+                    <a href="programs_management" type="button" class="btn btn-danger">Cancel</a>
+                    <button type="submit" class="btn bg-lime-600 hover:bg-lime-700 text-neutral-100 hover:text-neutral-100">Save</button>
+                </div>
             </form>
 
         </div>
